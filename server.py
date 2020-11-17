e96c1f8fa9fd976f452e08bf9d7653538f9ff66c
Mr. Karaaslan <developerkaraaslan@gmail.com>
2020-11-17 23:07:25 +0300

    template html for App

--- server.py
+++ server.py
@@ -2,10 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/sign_in")
 def sign_in_page():
     return render_template("Sign/sign_in.html")
 
+@app.route("/sign_up")
+def sign_up_page():
+    return render_template("Sign/sign_up.html")
+
+@app.route("/forgot_password")
+def forgot_password_page():
+    return render_template("Sign/forgot_password.html")
+
+@app.route("/")
+def search_events_page():
+    return render_template("App/search_events.html")
+
+@app.route("/attended_events")
+def attended_events_page():
+    return render_template("App/attended_events.html")
+
+@app.route("/profile")
+def profile_page():
+    return render_template("App/profile.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
